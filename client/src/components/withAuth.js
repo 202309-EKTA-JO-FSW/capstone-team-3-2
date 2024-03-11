c881227f31b35ea79afb9c6999b56f79e1e357a1
Sanad-Alshobaki <alshobakisanad@gmail.com>
2024-03-12 00:07:19 +0300

    fix the auth issue

---
+++ client/src/components/withAuth.js
@@ -0,0 +1,51 @@
+"use client"
+import { useRouter } from 'next/navigation';
+import React, { useState, useEffect } from 'react';
+
+const withAuth = (WrappedComponent) => {
+    return ({ ...props }) => {
+        const router = useRouter();
+        const [loading, setLoading] = useState(true);
+        const [authenticated, setAuthenticated] = useState(false);
+
+        useEffect(() => {
+            const isAuthenticated = () => {
+                if (typeof window !== 'undefined') {
+                    let token = localStorage.getItem('token');
+                    console.log(token);
+
+                    return token !== null;
+                }
+                return false;
+            };
+
+            const checkAuth = async () => {
+                if (!isAuthenticated()) {
+                    router.push('/auth/login');
+                } else {
+                    setLoading(false);
+                    setAuthenticated(true);
+                }
+            };
+
+            checkAuth();
+        }, [router]);
+
+        const signOut = () => {
+            localStorage.removeItem('token');
+            router.push('/auth/login');
+        };
+
+        if (loading) {
+            return <div>Loading...</div>;
+        }
+
+        if (!authenticated) {
+            return <div>Please sign in</div>;
+        }
+
+        return <WrappedComponent {...props} signOut={signOut} />;
+    };
+};
+
+export default withAuth;
