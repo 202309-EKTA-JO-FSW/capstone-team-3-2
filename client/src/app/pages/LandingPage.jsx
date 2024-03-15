6abaef3a616c08b1bbad02cc8887d18debdd53ad
Lubnabd <lubna.abdelkhaleq@gmail.com>
2024-03-15 14:33:35 +0300

    adding some updates

--- client/src/app/pages/LandingPage.jsx
+++ client/src/app/pages/LandingPage.jsx
@@ -14,30 +14,22 @@ const LandingPage = () => {
     console.log("searching", deliveryAddress);
   }
 
+  
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-8 relative">
 
       <div className=" flex flex-col relative">
         <div className="flex justify-start items-center w-full">
-          <h1 className="text-4xl font-bold text-black mb-8">Discover Our Most Delectable Dishes</h1>
-          <div className="photosContainer ml-auto">
-            <img src="https://barn2.com/wp-content/uploads/2020/09/834671_restaurant-chain-order-online_Op2_092120-cropped-1-820x358.png" alt="Food 1" className="slideIn-photo" />
-            <div className="rotating-photos-container absolute top-0 left-0">
-              
-            </div>
+          <div >
+<img src="https://assets.api.uizard.io/api/cdn/stream/509960c0-9679-42c2-9fe2-0e3a3ed5e54f.png" alt="discover1" className="relative top-16 left-36 w-10 h-10 rounded-lg bg-center bg-cover bg-no-repeat "/>
+          </div>
+          <div> <h1 className="text-6xl font-semibold font-poppins text-center text-black mb-8">Discover Our Most Delectable Dishes</h1>
+          </div>
+          <div >
+<img src="https://assets.api.uizard.io/api/cdn/stream/4c2ecf65-dcbe-4126-9aba-e90ab3514397.png" alt="discover2"  className="absolute top-446 left-1111 w-10 h-10 rounded-lg"/>
           </div>
         </div>
-      </div>
-
-
-      <div className="bg-gray-200 py-16 ">
-
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between w-full items-center">
-          <div className="text-center">
-
-            <h3 className="text-4xl font-bold text-black mb-8 flex items-center text-center">How It Works</h3>
-
-            <form onSubmit={handleSearchSubmit} className=" flex items-center ml-auto">
+        <form onSubmit={handleSearchSubmit} className=" flex items-center ml-auto">
               <input
                 type="text"
                 value={deliveryAddress}
@@ -48,27 +40,41 @@ const LandingPage = () => {
               />
               <button type="submit" className="bg-indigo-500 text-black px-4 py-2 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">Search</button>
             </form>
+      </div>
+
+
+      <div className="bg-gray-200 py-16 ">
+
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between w-full items-center">
+          <div className="text-center">
+
+            <h3 className="text-4xl font-bold text-black mb-8 flex items-center text-center">How It Works</h3>
+
+            
           </div>
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
 
-          <div className="bg-white p-8 rounded-lg border border-yellow-500 shadow-lg">
-            <img src="https://assets.api.uizard.io/api/cdn/stream/7dc79fea-dfab-43c4-8c3c-4d3c302573fb.png" alt="step 1" className="w-24 h-24 object-cover rounded-full" />
-            <h3 className="text-xl font-bold text-black mb-4">Step 1</h3>
-            <p className="text-gray-700">Enter your location, Simply type in your address or enable location services for a precise delivery</p>
-          </div>
-          <div className="bg-white p-8 rounded-lg border border-yellow-500 shadow-lg">
-            <img src="https://assets.api.uizard.io/api/cdn/stream/a46630bd-9a0c-4b7c-bb3c-6b0f4f2d3b31.png" alt="step 2" className="w-24 h-24 object-cover rounded-full" />
-            <h3 className="text-xl font-bold text-black mb-4">Step 2</h3>
-            <p className="text-gray-700">Choose a restaurant and a mouthwatering dish. The perfect quick bite is just a click away</p>
-          </div>
-          <div className="bg-white p-8 rounded-lg border border-yellow-500 shadow-lg">
-            <img src="https://assets.api.uizard.io/api/cdn/stream/6992293c-39ad-4d51-873e-64bf26754ab0.png" alt="step 3" className="w-24 h-24 object-cover rounded-full" />
-            <h3 className="text-xl font-bold text-black mb-4">Step 3</h3>
-            <p className="text-gray-700">Complete your payment. Sit back and relax while we prepare your delicious food</p>
-          </div>
-        </div>
+  <div className="bg-white p-8 rounded-lg border border-yellow-500 shadow-lg flex flex-col items-center">
+    <img src="https://assets.api.uizard.io/api/cdn/stream/7dc79fea-dfab-43c4-8c3c-4d3c302573fb.png" alt="step 1" className="w-24 h-24 object-cover rounded-lg mb-4" />
+    <h3 className="text-xl font-bold text-black mb-4">Step 1</h3>
+    <p className="text-gray-700">Enter your location, Simply type in your address or enable location services for a precise delivery</p>
+  </div>
+
+  <div className="bg-white p-8 rounded-lg border border-yellow-500 shadow-lg flex flex-col items-center">
+    <img src="https://assets.api.uizard.io/api/cdn/stream/a46630bd-9a0c-4b7c-bb3c-6b0f4f2d3b31.png" alt="step 2" className="w-24 h-24 object-cover rounded-full mb-4" />
+    <h3 className="text-xl font-bold text-black mb-4">Step 2</h3>
+    <p className="text-gray-700">Choose a restaurant and a mouthwatering dish. The perfect quick bite is just a click away</p>
+  </div>
+
+  <div className="bg-white p-8 rounded-lg border border-yellow-500 shadow-lg flex flex-col items-center">
+    <img src="https://assets.api.uizard.io/api/cdn/stream/6992293c-39ad-4d51-873e-64bf26754ab0.png" alt="step 3" className="w-24 h-24 object-cover rounded-full mb-4" />
+    <h3 className="text-xl font-bold text-black mb-4">Step 3</h3>
+    <p className="text-gray-700">Complete your payment. Sit back and relax while we prepare your delicious food</p>
+  </div>
+
+</div>
 
         <div className=" py-16">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -121,12 +127,10 @@ const LandingPage = () => {
 
 
       </div>
-    </div>
+    
 
 
-  );
-};
-
-
+ </div>
+  )}
 export default LandingPage;
 
--- client/src/components/LandingNavbar.jsx
+++ client/src/components/LandingNavbar.jsx
@@ -7,20 +7,20 @@ import { LoginButton, SignupButton } from "@/components/auth/login-button";
 
 const LandingNavbar = () => {
   return (
-    <nav className="bg-gray-800 h-16">
+    <nav className="bg-gray-800 h-20 py-8">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <div className="flex-shrink-0">
-              <span className="text-white mr-4 neon-text">DishDash</span>
+              <span className="font-bold font-poppins text-white mr-4 neon-text text-2xl leading-14">DishDash</span>
             </div>
           </div>
           <div className="flex">
             <LoginButton >
-              <Button variant='ghost' size='sm'>Sign in</Button>
+              <Button className="mr-4 cursor-pointer absolute top-18 left-60 w-36 h-16 px-2 border-0 rounded-lg bg-gray-900 text-white text-lg font-poppins font-semibold leading-7 focus:outline-none" variant='ghost' size='sm'>Sign in</Button>
             </LoginButton>
             <SignupButton >
-              <Button variant='ghost' size='sm'>Sign up</Button>
+              <Button className="cursor-pointer absolute top-18 left-72 w-36 h-16 px-2 border-0 rounded-lg bg-gray-900 text-white text-lg font-poppins font-semibold leading-7 focus:outline-none" variant='ghost' size='sm'>Sign up</Button>
             </SignupButton>
 
           </div>
