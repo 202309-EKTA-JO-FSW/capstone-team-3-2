600c873801edaa8d403f15a4590eb8cb01cb398c
wajd alkayyali <wajd.kayyali@gmail.com>
2024-03-15 14:50:56 +0300

    back to 3001/test

--- client/src/app/components/Test.jsx
+++ client/src/app/components/Test.jsx
@@ -5,7 +5,7 @@ const Hello = () => {
   const [testResult, setTestResult] = useState("");
 
   useEffect(() => {
-    fetch("http://localhost:3000")
+    fetch("http://localhost:3001/test")
       .then((res) => res.json())
       .then((data) => {
         setTestResult(data);
