6daf600323a439167412187f535625dc954f0f21
Lubnabd <lubna.abdelkhaleq@gmail.com>
2024-03-16 18:14:08 +0300

    updating landing page

--- client/src/app/pages/LandingPage.jsx
+++ client/src/app/pages/LandingPage.jsx
@@ -43,123 +43,124 @@ const LandingPage = () => {
             <img src="https://assets.api.uizard.io/api/cdn/stream/509960c0-9679-42c2-9fe2-0e3a3ed5e54f.png" alt="discover1" className="w-12 h-12 rounded-lg absolute right-0 bottom-0" />
           </div>
         </div>
+        
 
-        <div className="bg-gray-200 py-16 flex justify-center items-center w-full mb-8 relative">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between w-full items-center cursor-pointer" onClick={toggleSteps}>
-            <div className="text-center " >
-              <h1 className="text-4xl text-black mb-8 flex  items-center text-center text-black text-2xl font-semibold font-poppins" style={{ lineHeight: '48px' }}>How It Works</h1>
-            </div>
-          </div>
-          {showSteps && (
-           <div className="grid grid-cols-1 sm:grid-cols-3 gap-8">
-           <div className="bg-white p-8 rounded-lg border border-yellow-500 shadow-lg flex flex-row items-center">
-             <div className="mr-4">
-               <img src="https://assets.api.uizard.io/api/cdn/stream/7dc79fea-dfab-43c4-8c3c-4d3c302573fb.png" alt="step 1" className="w-12 h-12 object-cover rounded-lg " />
-             </div>
-             <div className="flex flex-col">
-               <h3 className="text-xl font-semibold text-black mb-2">Step 1</h3>
-               <p className="text-gray-700">Enter your location, Simply type in your address or enable location services for a precise delivery</p>
-             </div>
-           </div>
-           
-           <div className="bg-white p-8 rounded-lg border border-yellow-500 shadow-lg flex flex-row items-center">
-  <div className="mr-4">
-    <img src="https://assets.api.uizard.io/api/cdn/stream/a46630bd-9a0c-4b7c-bb3c-6b0f4f2d3b31.png" alt="step 2" className="w-12 h-12 object-cover rounded-full mb-4" />
+        <div className="bg-gray-200 py-16   mb-8 relative">
+  <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex-col   cursor-pointer" onClick={toggleSteps}>
+    <div className="text-center">
+      <h1 className="text-4xl text-black mb-8 flex items-center text-center text-black text-2xl font-semibold font-poppins" style={{ lineHeight: '48px' }}>How It Works</h1>
+    </div>
   </div>
-  <div className="flex flex-col">
-    <h3 className="text-xl font-semibold text-black mb-2">Step 2</h3>
-    <p className="text-gray-700">Choose a restaurant and a mouthwatering dish. The perfect quick bite is just a click away</p>
-  </div>
-</div>
-           
-           <div className="bg-white p-8 rounded-lg border border-yellow-500 shadow-lg flex flex-row items-center">
-            <div>
-             <img src="https://assets.api.uizard.io/api/cdn/stream/6992293c-39ad-4d51-873e-64bf26754ab0.png" alt="step 3" className="w-12 h-12 object-cover rounded-full mb-4" />
-             </div>
-             <div>
-             <h3 className="text-xl font-semibold text-black mb-2">Step 3</h3>
-             <p className="text-gray-700">Complete your payment. Sit back and relax while we prepare your delicious food</p>
-             </div>
-           </div>
-         </div>
-         
-            
-          )}
+  {showSteps && (
+    <div className="flex flex-cols-3 gap-8">
+      <div className="bg-white p-8 rounded-lg border border-yellow-500 shadow-lg flex items-start">
+        <div className="mr-4">
+          <img src="https://assets.api.uizard.io/api/cdn/stream/7dc79fea-dfab-43c4-8c3c-4d3c302573fb.png" alt="step 1" className="w-12 h-12 object-cover rounded-lg" />
         </div>
+        <div>
+          <h3 className="text-xl font-semibold text-black mb-2">Step 1</h3>
+          <p className="text-gray-700">Enter your location, Simply type in your address or enable location services for a precise delivery</p>
+        </div>
+      </div>
 
-        <div className=" py-16  ">
-          <div className="  flex flex-col justify-center items-center w-full mb-8 relative  ">
-            <div className="text-center">
-              <div>
-              <h1 className="text-4xl font-semibold text-black mb-8">Join The DishDash Community</h1>
+      <div className="bg-white p-8 rounded-lg border border-yellow-500 shadow-lg flex items-start">
+        <div className="mr-4">
+          <img src="https://assets.api.uizard.io/api/cdn/stream/a46630bd-9a0c-4b7c-bb3c-6b0f4f2d3b31.png" alt="step 2" className="w-12 h-12 object-cover rounded-full mb-4" />
+        </div>
+        <div>
+          <h3 className="text-xl font-semibold text-black mb-2">Step 2</h3>
+          <p className="text-gray-700">Choose a restaurant and a mouthwatering dish. The perfect quick bite is just a click away</p>
+        </div>
+      </div>
+
+      <div className="bg-white p-8 rounded-lg border border-yellow-500 shadow-lg flex items-start">
+        <div>
+          <img src="https://assets.api.uizard.io/api/cdn/stream/6992293c-39ad-4d51-873e-64bf26754ab0.png" alt="step 3" className="w-12 h-12 object-cover rounded-full mb-4" />
+        </div>
+        <div>
+          <h3 className="text-xl font-semibold text-black mb-2">Step 3</h3>
+          <p className="text-gray-700">Complete your payment. Sit back and relax while we prepare your delicious food</p>
+        </div>
+      </div>
+    </div>
+  )}
 </div>
 
-<div>
-  <div className="bg-white p-8 rounded-lg shadow-md flex flex-row items-center border border-yellow-500 shadow-lg">
-    <img src="https://assets.api.uizard.io/api/cdn/stream/e246370c-8563-4926-bbe9-d1d44b872ab7.png" alt="become a rider" className="w-12 h-12 object-cover rounded-full mr-4" />
+<div className="text-black font-bold">
+  <div className="flex flex-col text-black">
     <div>
-      <h3 className="text-xl font-semibold text-black mb-2">Become A Rider</h3>
-      <p className="text-gray-700 mb-4">Earn money by delivering food from restaurants. All you need is a bike and a passion for great service</p>
-      <a href="https://docs.google.com/forms/d/e/1FAIpQLSfozeHDuj3aPCKtKCIZbD9JdO4h6eQMXKJcS0GdTrugABOwTg/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer">
-        <button type="button" className="font-semibold border border-black bg-black text-black px-4 py-2 rounded-md hover:bg-gray-900 focus:outline-none focus:bg-gray-900">Start Riding</button>
-      </a>
+      <h1 className="text-center text-4xl font-semibold text-black mb-8">Join The DishDash Community</h1>
+    </div>
+    <div className="flex flex-cols-3 gap-8">
+    <div className="mr-4 bg-white p-8 rounded-lg shadow-md flex flex-col items-start border border-yellow-500 shadow-lg">
+  <img src="https://assets.api.uizard.io/api/cdn/stream/e246370c-8563-4926-bbe9-d1d44b872ab7.png" alt="become a rider" className="w-10 h-12 object-cover rounded-full mb-4" />
+  <div>
+    <h3 className="text-xl font-semibold text-black mb-2">Become A Rider</h3>
+    <p className="text-gray-700 mb-4">Earn money by delivering food from restaurants. All you need is a bike and a passion for great service</p>
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSfozeHDuj3aPCKtKCIZbD9JdO4h6eQMXKJcS0GdTrugABOwTg/viewform?usp=sf_link" target="_blank" rel="noopener noreferrer">
+      <button type="button" className="font-semibold border border-black bg-black text-black px-4 py-2 rounded-md hover:bg-gray-900 focus:outline-none focus:bg-gray-900">Start Riding</button>
+    </a>
+  </div>
+</div>
+
+<div className="mr-4 bg-white p-8 rounded-lg shadow-md flex flex-col items-start border border-yellow-500 shadow-lg">
+  <img src="https://assets.api.uizard.io/api/cdn/stream/3de66382-a1d7-41e9-add0-73355e305e95.png" alt="partner with us" className="w-10 h-14 object-cover rounded-full mb-4" />
+  <div>
+    <h3 className="text-xl font-semibold text-black mb-2">Partner with Us</h3>
+    <p className="text-gray-700 mb-4">DishDash helps restaurants thrive with online ordering, loyalty programs, and more</p>
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSdwVPLMm-BxGPulP1lEoipVLSAgihCjQ4TrC2gt8QVNt25OMA/viewform" target="_blank" rel="noopener noreferrer">
+      <button type="button" className="font-semibold border border-black bg-black text-black px-4 py-2 rounded-md hover:bg-gray-900 focus:outline-none focus:bg-gray-900">Join Now</button>
+    </a>
+  </div>
+</div>
+
+<div className=" mr-4 bg-white p-8 rounded-lg shadow-md flex flex-col items-start border border-yellow-500 shadow-lg">
+  <img src="https://assets.api.uizard.io/api/cdn/stream/5731af9d-856a-4f19-be88-ace58e953529.png" alt="join our team" className="w-12 h-12 object-cover rounded-full mb-4" />
+  <div>
+    <h3 className="text-xl font-semibold text-black mb-2">Join Our Team</h3>
+    <p className="text-gray-700 mb-4 ">Be part of a team that's building an exceptional delivery service</p>
+    <a href="https://docs.google.com/forms/d/e/1FAIpQLSdwVPLMm-BxGPulP1lEoipVLSAgihCjQ4TrC2gt8QVNt25OMA/viewform" target="_blank" rel="noopener noreferrer">
+      <button type="button" className="font-semibold border border-black bg-black text-black px-4 py-2 rounded-md hover:bg-gray-900 focus:outline-none focus:bg-gray-900">Work With Us</button>
+    </a>
+  </div>
+</div>
     </div>
   </div>
 </div>
 
-   <div>
-     <div className="bg-white p-8 rounded-lg shadow-md flex flex-row items-center border border-yellow-500 shadow-lg">
-                  <img src="https://assets.api.uizard.io/api/cdn/stream/3de66382-a1d7-41e9-add0-73355e305e95.png" alt="partner with us" className="w-10 h-10 object-cover rounded-full mr-4" />
-                  <div>
-                  <h3 className="text-xl font-semibold text-black mb-2">Partner with Us</h3>
-                  <p className="text-gray-700 mb-4">DishDash helps restaurants thrive with online ordering, loyalty programs, and more</p>
-                  <a href="https://docs.google.com/forms/d/e/1FAIpQLSdwVPLMm-BxGPulP1lEoipVLSAgihCjQ4TrC2gt8QVNt25OMA/viewform" target="_blank" rel="noopener noreferrer">
-        <button type="button" className="font-semibold border border-black bg-black text-black px-4 py-2 rounded-md hover:bg-gray-900 focus:outline-none focus:bg-gray-900">Join Now</button>
-      </a>
-                </div>
-</div>
-</div>
 
-<div>
-                <div className="bg-white p-8 rounded-lg shadow-md flex flex-row items-center border border-yellow-500 shadow-lg">
-                  <img src="https://assets.api.uizard.io/api/cdn/stream/5731af9d-856a-4f19-be88-ace58e953529.png" alt="join our team" className="w-10 h-10 object-cover rounded-full mr-4" />
-                  <div>
-                  <h3 className="text-xl font-semibold text-black mb-2">Join Our Team</h3>
-                  <p className="text-gray-700 mb-4">Be part of a team that's building an exceptional delivery service</p>
-                  <a href="https://docs.google.com/forms/d/e/1FAIpQLSdwVPLMm-BxGPulP1lEoipVLSAgihCjQ4TrC2gt8QVNt25OMA/viewform" target="_blank" rel="noopener noreferrer">
-        <button type="button" className="font-semibold border border-black bg-black text-black px-4 py-2 rounded-md hover:bg-gray-900 focus:outline-none focus:bg-gray-900">Work With Us</button>
-      </a>
-                </div>
-              </div>
-            </div>
-          
+    
 
-
-        </div>
-
-        <div className="bg-white p-8 rounded-lg shadow-md flex flex-row items-center w-full border border-yellow-500 shadow-lg">
+<div className="bg-white p-8 rounded-lg shadow-md flex flex-row items-center w-full border border-yellow-500 shadow-lg">
+  <div className="flex-1">
+    <h1 className="text-xl font-semibold text-black mb-2 mr-4">Download Our App</h1>
+    <div className="flex items-center justify-between">
+      <div className="mr-4 ">
+        <img src="https://assets.api.uizard.io/api/cdn/stream/07d4696b-4d8a-41d4-b1d8-d76970325da7.png" alt="download" />
+      </div>
+      <div>
+        <h2 className="font-semibold">Order With Ease </h2>
+        <p>Indulge in the most delicious bites delivered to your door with FoodieHub. Our speedy delivery service offers a variety of cuisines from local restaurants</p>
+        <form className="w-full">
           <div>
-          <h3 className="text-xl font-bold text-black mb-2">Download Our App</h3>
-</div>
-<div>
-          <form className="w-full">
-            <div>
-
-            </div>
-            <div>
             <button
               type="download"
-              className="w-full bg-indigo-500 text-white px-4 py-2 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
-            >Download</button>
-            </div>
-          </form>
+              className="w-full bg-indigo-500 text-black px-4 py-2 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
+            >
+              Download
+            </button>
           </div>
-        </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
 
-     </div>
-</div>
-</div>
-</div>
+        </div> 
+
+        </div>
+       
+ 
   )};
 
 export default LandingPage;
