60debe083a7cc1783fb5766e06485df88f82d5b0
Lubnabd <lubna.abdelkhaleq@gmail.com>
2024-03-25 04:53:59 +0300

    done

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
 
@@ -11,15 +12,15 @@ const Footer = () => {
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-0 md:mb-0 ">
             
-            <Link href="/pages/AboutPage" passHref>
+            <Link href="/pages/AboutPage">
               <span className="mr-4 cursor-pointer">About Us</span>
             </Link>
             
-            <Link href="/pages/ContactUs" passHref>
+            <Link href="/pages/ContactUs" >
               <span className="mr-4 cursor-pointer">Contact Us</span>
             </Link>
            
-            <Link href="/auth/register/restaurant" passHref>
+            <Link href="/auth/register/restaurant" >
               <span className="cursor-pointer">Join Us</span>
             </Link>
           </div>
@@ -30,51 +31,61 @@ const Footer = () => {
       </div>
       <div className="mb-0 mt-2 flex justify-center">
           <div className="mb-0 mr-4 flex items-center justify-between ml-4 mt-0 text-center">
-            <div className="mb-2  mr-4 mt-0" >
-            <p>Lubna Abdelkhaliq</p>
-            <a href="https://www.linkedin.com/in/lubna-abdelkhaliq-09020a18b/" className="mr-2" target="_blank" rel="noopener noreferrer">
-              LinkedIn
-            </a>
-            <a href="https://github.com/Lubnaabd" target="_blank" rel="noopener noreferrer" className='pr-5'>
-              GitHub
-            </a>
+            <div className="mb-2  mr-4 mt-0 text-center">
+                <p>Lubna Abdelkhaliq</p>
+                <div className="mt-2 flex">
+                    <a href="https://www.linkedin.com/in/lubna-abdelkhaliq-09020a18b/" className="mr-2" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin size={20} />
+                    </a>
+                    <a href="https://github.com/Lubnaabd" target="_blank" rel="noopener noreferrer" className='pr-5'>
+                        <FaGithub size={20} />
+                    </a>
 </div>
-            <div className="mb-2 mr-4 ">
-            <p>Ahmed Shalash</p>
-            <a href="https://www.linkedin.com/in/matt-shal/" className="mr-2" target="_blank" rel="noopener noreferrer">
-              LinkedIn
-            </a>
-            <a href="https://github.com/mattshal1" target="_blank" rel="noopener noreferrer" className='pr-5'>
-              GitHub
-            </a>
 </div>
-<div className="mb-2 mr-4">
-            <p>Farah Arar</p>
-            <a href="" className="mr-2" target="_blank" rel="noopener noreferrer">
-              LinkedIn
-            </a>
-            <a href="" target="_blank" rel="noopener noreferrer" className='pr-5'>
-              GitHub
-            </a>
+            <div className="mb-2 mr-4 text-center">
+                <p>Ahmad Shalash</p>
+                <div className="mt-2 flex">
+                    <a href="https://www.linkedin.com/in/matt-shal/" className="mr-2" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin size={20} />
+                    </a>
+                    <a href="https://github.com/mattshal1" target="_blank" rel="noopener noreferrer" className='pr-5'>
+                        <FaGithub size={20} />
+                    </a>
 </div>
-<div className="mb-2 mr-4">
-            <p>Sanad Alshobaki</p>
-            <a href="" className="mr-2" target="_blank" rel="noopener noreferrer">
-              LinkedIn
-            </a>
-            <a href="" target="_blank" rel="noopener noreferrer" className='pr-5'>
-              GitHub
-            </a>
+</div>
+<div className="mb-2 mr-4 text-center">
+                <p>Sanad Elshobaki</p>
+                <div className="mt-2 flex">
+                    <a href="https://www.linkedin.com/in/sanad-alshobaki/" className="mr-2" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin size={20} />
+                    </a>
+                    <a href="https://github.com/sanad-alshobaki" target="_blank" rel="noopener noreferrer" className='pr-5'>
+                        <FaGithub size={20} />
+                    </a>
+</div>
+</div>
+<div className="mb-2 mr-4 text-center">
+                <p>Farah Arar</p>
+                <div className="mt-2 flex">
+                    <a href="" className="mr-2" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin size={20} />
+                    </a>
+                    <a href="" target="_blank" rel="noopener noreferrer" className='pr-5'>
+                        <FaGithub size={20} />
+                    </a>
+</div>
 </div>
 
-<div className="mb-2 mr-4">
-            <p>Wajd Kilani</p>
-            <a href="" className="mr-2" target="_blank" rel="noopener noreferrer">
-              LinkedIn
-            </a>
-            <a href="" target="_blank" rel="noopener noreferrer" className='pr-5'>
-              GitHub
-            </a>
+<div className="mb-2 mr-4 text-center">
+                <p>Wajd Alkilani</p>
+                <div className="mt-2 flex">
+                    <a href="" className="mr-2" target="_blank" rel="noopener noreferrer">
+                        <FaLinkedin size={20} />
+                    </a>
+                    <a href="" target="_blank" rel="noopener noreferrer" className='pr-5'>
+                        <FaGithub size={20} />
+                    </a>
+</div>
 </div>
         </div>
       </div>
